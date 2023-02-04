f2844ccab2f80628d8eec0e780e3c9756b4f8638
Mary8992 <mc046569@gmail.com>
2023-02-04 19:43:53 +0300

    time and date functionality added

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
+//main div:Date and Time
+let time = document.getElementById("time");
+let date = document.getElementById("date");
+
+setInterval(update, 1000);
+
+function update() {
+    let now = new Date();
+    let seconds = now.getSeconds();
+    let minutes = now.getMinutes();
+    let hours = now.getHours();
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    let timeString = `${hours}:${minutes}:${seconds}`;
+    time.innerHTML = timeString;
+
+    /*
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  options.timeZoneName = 'short';
+  console.log(date.toLocaleDateString('en-US', options));
+ */
+
+    let weekDay = now.toLocaleDateString("en-US", { weekday: "long" });
+    let monthDay = now.toLocaleDateString("en-US", { day: "numeric" });
+    let year = now.toLocaleDateString("en-US", { year: "numeric" });
+    let monthName = now.toLocaleDateString("en-US", { month: "long" });
+
+    let dateString = `${weekDay}-${monthName} ${monthDay},${year}`;
+
+    date.innerHTML = dateString;
+}
+
 //container div:To Do List
 let push = document.querySelector("#push");
 
